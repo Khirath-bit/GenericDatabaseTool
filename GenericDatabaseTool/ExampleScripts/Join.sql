30fbf592b980b2668675d269533f5b14952d19da
Thomas Krüssel <thomas.kruessel@emp.de>
2020-12-16 16:35:24 +0100

    Fixed Join example

--- GenericDatabaseTool/ExampleScripts/Join.sql
+++ GenericDatabaseTool/ExampleScripts/Join.sql
@@ -1,21 +1,8 @@
-﻿SELECT --Inner join example. 
+﻿SELECT
     *
 FROM
     TABLENAME t1
 INNER JOIN
-    TABLENAME t2
-ON
-    t1.attr = t2.attr
-WHERE
-    t1.attr = 2;
-
-------------------------------------------------
-
-SELECT --Left join example.
-    *
-FROM
-    TABLENAME t1
-LEFT JOIN
     TABLENAME t2
 ON
     t1.attr = t2.attr
